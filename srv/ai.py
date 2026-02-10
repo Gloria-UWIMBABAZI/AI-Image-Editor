be5d38984bdda73970b5860f4f596aa94ed3dbb1
Ozpyn <74064734+Ozpyn@users.noreply.github.com>
2026-02-10 10:53:37 -0500

    inpaint and outpaint endpoints
    implemented the base feature for running inpaint and outpaint, outpaint being a convolution of inpainting

--- srv/ai.py
+++ srv/ai.py
@@ -1,17 +1,53 @@
 import torch
+from PIL import Image
 from diffusers.utils import load_image
+from diffusers import AutoPipelineForInpainting
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+inpainting_pipe = AutoPipelineForInpainting.from_pretrained(
+    "kandinsky-community/kandinsky-2-2-decoder-inpaint",
+    torch_dtype=torch.float16
+).to(DEVICE)
+
+inpainting_pipe.enable_xformers_memory_efficient_attention()
 
 # These might be changed later to implement Redis, which would allow us to queue jobs.
 # We could also 'lazy load' each model and keep it in memory for as long as the Flask app is running, making it so subsequent requests are faster.
 
-def run_inpaint(image, mask):
-    # Call and Run model here
-    return
+def run_inpaint(image, mask, prompt=None):
+    prompt = prompt or ""
+    guidance = 1.0 if prompt == "" else 4.0
 
-def run_outpaint(image, directions):
-    # Call and Run
-    # Modify images to add borders and run this as a convolution of inpaint
-    return run_inpaint(mod_image, new_mask)
+    with torch.inference_mode():
+        result = inpainting_pipe(
+            prompt=prompt,
+            image=image,
+            mask_image=mask,
+            guidance_scale=0.0 if prompt == "" else 4.0,
+            num_inference_steps=40,
+        )
+    return result.images[0]
+
+
+def run_outpaint(image, directions, prompt=None):
+    w, h = image.size
+    
+    x = directions.get("x", 0)
+    y = directions.get("y", 0)
+
+    left = directions.get("left", x)
+    right = directions.get("right", x)
+    top = directions.get("top", y)
+    bottom = directions.get("bottom", y)
+
+    mod_image = Image.new("RGB", (w + left + right, h + top + bottom))
+    mod_image.paste(image, (left, top))
+
+    new_mask = Image.new("L", mod_image.size, 255)
+    new_mask.paste(Image.new("L", (w, h), 0), (left, top))
+
+    return run_inpaint(mod_image, new_mask, prompt)
 
 def run_deblur(image):
     # Call and Run
